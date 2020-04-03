eab9e70ec9aa9efb5d53c8f548b761b50f6b7680
B-R-H <barnaby66@gmail.com>
2020-04-03 18:58:47 +0100

    base app working

--- services/python/particle_service/application/routes.py
+++ services/python/particle_service/application/routes.py
@@ -6,28 +6,28 @@ import requests
 partilces = []
 
 #trigger for reset of partilces on request 
-@app.route('/generate')
+@app.route("/generate")
 def generate():
 	global partilces
 	partilces = []
 	for i in range(100):
-		partilces.append(Particle.Particle(xuper=100,xlower=-100,yuper=100,ylower=-100))
+		partilces.append(Particle.Particle(xuper=75,xlower=-75,yuper=75,ylower=-75))
 	return "partilces generated"
 
-@app.route('/move', methods = ['GET','POST'])
+@app.route("/move", methods = ["GET","POST"])
 def move():
 
 	global partilces
 	if len(partilces)!=100:
-		return 'partilces not generated correctly'
+		return "partilces not generated correctly"
 
-	flag = Run_state.query.filter_by(flag='running particles').first()
+	flag = Run_state.query.filter_by(flag="running particles").first()
 	run_state = flag.state
 	global_max = None
 
 	while run_state:
 		# pulls from the databse to see if it should be running.
-		flag = Run_state.query.filter_by(flag='running particles').first()
+		flag = Run_state.query.filter_by(flag="running particles").first()
 		run_state = flag.state
 		partile_location={}
 		particle_data={}
@@ -35,18 +35,19 @@ def move():
 			if global_max!=None:
 				partilces[i].update(global_max[0],global_max[1])
 			partile_location[i]={
-				'x':partilces[i].get_postion_x(),
-				'y':partilces[i].get_postion_y()
+				"x":partilces[i].get_postion_x(),
+				"y":partilces[i].get_postion_y()
 			}
 			particle_data[i]={
-				'x':partilces[i].get_postion_x(),
-				'y':partilces[i].get_postion_y(),
-				'xv':partilces[i].get_velosity_x(),
-				'yv':partilces[i].get_velosity_y()
+				"x":partilces[i].get_postion_x(),
+				"y":partilces[i].get_postion_y(),
+				"xv":partilces[i].get_velosity_x(),
+				"yv":partilces[i].get_velosity_y()
 			}
-		gmax_responce = requests.post('http://logic:5002/logic',json=partile_location)
-		responce = requests.post('http://graphing:5001/plot',json=particle_data)
-		global_max=[gmax_responce['x'],gmax_responce['y']]
+		gmax_responce = requests.post("http://logic:5002/logic",json=partile_location)
+		requests.post("http://graphing:5001/plot",json=particle_data)
+		gmax_responce = gmax_responce.json()
+		global_max=[gmax_responce["x"],gmax_responce["y"]]
 	return "run state not true"
 
         
