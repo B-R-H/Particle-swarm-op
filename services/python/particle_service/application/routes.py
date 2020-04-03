f37d2bdc69a5981492290ca9c1aac758a045bea1
B-R-H <barnaby66@gmail.com>
2020-04-03 17:09:23 +0100

    particle service complete

--- services/python/particle_service/application/routes.py
+++ services/python/particle_service/application/routes.py
@@ -1,6 +1,7 @@
-from application import app, db
+from application import app, db, Particle
 from flask import url_for, request, jsonify
-import Particle
+from application.models import *
+import requests
 
 partilces = []
 
@@ -16,38 +17,36 @@ def generate():
 @app.route('/move', methods = ['GET','POST'])
 def move():
 
+	global partilces
+	if len(partilces)!=100:
+		return 'partilces not generated correctly'
+
 	flag = Run_state.query.filter_by(flag='running particles').first()
 	run_state = flag.state
-	global_max =None
+	global_max = None
 
 	while run_state:
 		# pulls from the databse to see if it should be running.
 		flag = Run_state.query.filter_by(flag='running particles').first()
 		run_state = flag.state
-		global partilces
-	    partile_location={}
-	    particle_data={}
-	    for i in range(len(partilces)):
-	    	if global_max!=None:
-	       		particles[i].update(global_max[0],global_max[1])
-	       	partile_location[i]={
-	       		'x':particles[i].get_postion_x(),
-	       		'y':particles[i].get_postion_y()
-	       	}
-	       	particle_data[i]={
-	       		'x':particles[i].get_postion_x(),
-	       		'y':particles[i].get_postion_y(),
-	       		'xv':particles[i].get_velosity_x(),
-	        	'yv':particles[i].get_velosity_y()
-	      	}
-	    ljson = jsonify(partile_location)
-	    djson = jsonify(particle_data)
-	    # post request to logic function that will return global max
-	    # post request to graphing function to update postions
-	    content = request.get_json()
-	   	global_max=[content['x'],content['y']]
-	    
-	    
-    return "run state not true"
+		partile_location={}
+		particle_data={}
+		for i in range(len(partilces)):
+			if global_max!=None:
+				partilces[i].update(global_max[0],global_max[1])
+			partile_location[i]={
+				'x':partilces[i].get_postion_x(),
+				'y':partilces[i].get_postion_y()
+			}
+			particle_data[i]={
+				'x':partilces[i].get_postion_x(),
+				'y':partilces[i].get_postion_y(),
+				'xv':partilces[i].get_velosity_x(),
+				'yv':partilces[i].get_velosity_y()
+			}
+		gmax_responce = requests.post('http://logic:5002/logic',json=partile_location)
+		responce = requests.post('http://graphing:5001/plot',json=particle_data)
+		global_max=[gmax_responce['x'],gmax_responce['y']]
+	return "run state not true"
 
         
