f8665cd7ab772c0647ec98cd247cfe6902f80b20
B-R-H <barnaby66@gmail.com>
2020-04-15 16:44:55 +0100

    set up configuration for kuberneties deployment

--- services/python/front_end/application/routes.py
+++ services/python/front_end/application/routes.py
@@ -13,6 +13,10 @@ def start():
 	db.session.commit()
 	# try block needed to stop the intentional time out from causing trouble on the front end.
 	try:
+		requests.get("http://graphing:5001/plot",timeout=5)
+	except:
+		pass
+	try:
 		requests.get("http://party:5003/move",timeout=1)
 	except:
 		pass
@@ -43,5 +47,6 @@ def home():
 		gmax = [data[data_length].max_value,data[data_length].x,data[data_length].y]
 	else:
 		gmax =[None,None,None]
-	# button to start and stop process
-	return render_template('home.html', title='Particle Swarm Optermisation', fx='z=5*sin(x/10)+5*cos(y/10)+5*sin(e^(y^2/10)+e^(x^2/10))*cos(x*y))-(0.001*(x^2+y^2)', gmax=gmax, fig_ip=os.getenv('FIG_IP'))
+	flag = Run_state.query.filter_by(flag="running particles").first()
+	run_state = flag.state
+	return render_template('home.html', title='Particle Swarm Optermisation', fx='z=5*sin(x/10)+5*cos(y/10)+5*sin(e^(y^2/10)+e^(x^2/10))*cos(x*y))-(0.001*(x^2+y^2)', gmax=gmax, fig_ip=os.getenv('FIG_IP'),running=run_state)
